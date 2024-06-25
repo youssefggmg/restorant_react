90f60de5de59d1c4dc35074e6334854e20e86464
youssef-elhoubi <elhoubiyoussef@gmail.com>
2024-06-25 03:34:57 +0100

    modifide the navbare andadd the hero section and add some other things

---
+++ src/NavBar.jsx
@@ -0,0 +1,59 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import "./style/nav.css";
+import "./style/globale.css"
+import 'font-awesome/css/font-awesome.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
+
+
+function Navbare() {
+    return (
+        <>
+            {['sm'].map((expand) => (
+                <Navbar key={expand} expand={expand} className="bg-body-tertiary mb-3" >
+                    <Container fluid>
+                        <Navbar.Brand href="#"><img src="react-restaurant-high-resolution-logo-black-transparent.png" alt="react restaurent" width={"190em"} /></Navbar.Brand>
+                        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+                        <Navbar.Offcanvas
+                            id={`offcanvasNavbar-expand-${expand}`}
+                            aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+                            placement="end"
+                        >
+                            <Offcanvas.Header closeButton>
+                                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                                    React Reastaurant
+                                </Offcanvas.Title>
+                            </Offcanvas.Header>
+                            <Offcanvas.Body>
+                                <Nav className="justify-content-end flex-grow-1 pe-3">
+                                    <Nav.Link href="#action2" className='bolde'>Add meal</Nav.Link>
+                                    <Nav.Link href="#action1" className='bolde'>Home</Nav.Link>
+                                    <Nav.Link href="#action2" className='bolde'>about</Nav.Link>
+                                </Nav>
+                                <Form className="d-flex">
+                                    <Form.Control
+                                        type="search"
+                                        placeholder="Search"
+                                        className="me-2"
+                                        aria-label="Search"
+                                    />
+                                    <Button variant="outline-success" className='search-button'> <FontAwesomeIcon icon={faMagnifyingGlass}  />Search</Button>
+                                </Form>
+                            </Offcanvas.Body>
+                        </Navbar.Offcanvas>
+                    </Container>
+                </Navbar>
+            ))}
+        </>
+    );
+}
+
+export default Navbare;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Navbare from './NavBar.jsx';
+import Hero from './Hero.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Navbare/>
+    <Hero/>
   </React.StrictMode>,
 )
